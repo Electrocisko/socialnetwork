e2fec45c4fff749b5ef11754e79f0322ebd2fa33
Francisco Zuidwijk <electrocisko@gmail.com>
2023-03-05 18:34:53 -0300

    index.js-type module

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-// const connection = require ("./database/connection.js");
 import connection from "./database/connection.js";
+import express from "express";
+import cors from "cors";
 
 console.log("API NODE JS")
 
 connection();
+
+//Mensaje de bienvenida
+console.log("API NODE JS");
+
+//Conexion a base de datos;
+connection();
+
+//Crear servidor Node
+const PORT = 8080;
+const app = express();
+
+//Cors
+app.use(cors());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+// ruta de prueba 
+app.use('/',(req,res) => {
+    return res.status(200).json({
+        "id": "1",
+        "nombre": "Francisco",
+        "web": "electrocisko.com.ar"
+    })
+});
+
+// hacer escuchar el servidor
+const server = app.listen(PORT, () => {
+    console.log(`Servidor levantado en http://localhost:${PORT}`)
+});
