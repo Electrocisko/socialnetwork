a8978b09b7d90a4e8aae77003ae8689fccde7c80
Francisco Zuidwijk <electrocisko@gmail.com>
2023-08-14 16:47:39 -0300

    trabajando en follows

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,3 +1,7 @@
+const Follow = require("../models/follow.js");
+const User = require("../models/user.js");
+
+
 // Acciones de prueba
 const pruebaFollow  = (req, res) => {
     return res.status(200).send({
@@ -5,7 +9,42 @@ const pruebaFollow  = (req, res) => {
     })
 }
 
+
+//Accion de guardar un follow
+const save = (req,res) => {
+    // conseguir datos por body
+    let data = req.body;
+
+    console.log(data)
+
+    //sacar el id del usuario
+
+    //crear objeto con el modelo follow
+
+    // Guardar objeto en la base de datos
+
+
+
+
+
+    return  res.status(200).json({
+        status: "succes",
+        message: "Salvado",
+        identity: req.user
+    })
+}
+
+
+//Accion de borrar un follow
+
+
+// Accion de listar listado que sigo
+
+
+// Accion de listar seguidores
+
 // Exportar acciones
 module.exports = {
-    pruebaFollow
+    pruebaFollow,
+    save
 }
--- routes/follow.js
+++ routes/follow.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const followControler = require('../controllers/follow.js');
+const check = require('../middlewars/auth.js');
 
 // Defino la rutas
-
-router.get('/follow', followControler.pruebaFollow );
+router.get('/prueba-follow', followControler.pruebaFollow);
+router.post('/save',  check.auth,followControler.save );
 
 module.exports = router;
