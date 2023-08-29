f38669e7a187dc743f3769390a76b3b3d295a75e
Francisco Zuidwijk <electrocisko@gmail.com>
2023-08-29 19:16:42 -0300

    se agrego multer en publications

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,4 +1,5 @@
 const Publication = require("../models/publication.js");
+const mongoose = require("mongoose");
 
 // Acciones de prueba
 const pruebaPublication = (req, res) => {
@@ -38,37 +39,143 @@ const save = async (req, res) => {
 };
 
 // Obtener una publicacion en concreto
-const detail = async (req,res) => {
-    try {
-        // Obtener el id de la publicacion
-        const publicactionId = req.params.id;
-        //hacer un find con ese id
-       
-       let publicationStored = await Publication.findById(publicactionId)
-        // devolver la respuesta
+const detail = async (req, res) => {
+  try {
+    // Obtener el id de la publicacion
+    const publicactionId = req.params.id;
+    //hacer un find con ese id
 
-        return res.status(200).json({
-            status: "success",
-            message: "Aca muestro la publicacion en concreto",
-            publicationStored
-        })
-    } catch (error) {
-        return res.status(400).json({
-            status: "error",
-            message: "No se ha podido obtener detail",
-            error: error
-          });
+    let publicationStored = await Publication.findById(publicactionId);
+    // devolver la respuesta
+    return res.status(200).json({
+      status: "success",
+      message: "Aca muestro la publicacion en concreto",
+      publicationStored,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: "error",
+      message: "No se ha podido obtener detail",
+      error: error,
+    });
+  }
+};
+
+//Listar publicaciones de un usuario
+
+const user = async (req, res) => {
+  try {
+    //Obtengo el id por url
+    const userId = req.params.id;
+    //Obtengo la pagina
+    let page = req.params.page ? parseInt(req.params.page) : 1;
+    const itemPerPage = 5;
+
+    //Find , populate, ordenar y paginar
+    validId = mongoose.isValidObjectId(userId);
+    if (!validId || userId == null) {
+      return res.status(400).json({
+        status: "error",
+        message: "Id no valido o nulo",
+      });
     }
 
-}
+    await Publication.find({ user: userId })
+      .sort("-created_at")
+      .populate("user", "-password -__v -role")
+      .paginate(page, itemPerPage, (error, publications, total) => {
+        return res.status(200).json({
+          status: "success",
+          message: "Listado de publicaciones ",
+          publications,
+          page,
+          total,
+          pages: Math.ceil(total / itemPerPage),
+        });
+      });
+  } catch (error) {
+    return res.status(400).json({
+      status: "error",
+      message: "No se ha podido obtener publicaciones",
+      error: error,
+    });
+  }
+};
 
-//Listar todas las publicaciones de usarios que sigo
-
-// Listar publicaciones de un usario en concreto
+// Listar  todas las publicaciones (FEED)
 
 // Eliminar publicaciones
+const remove = async (req, res) => {
+  try {
+    const publicactionId = req.params.id;
+    let publicationRemoved = await Publication.find({
+      user: req.user.id,
+      _id: publicactionId,
+    }).remove();
+
+    if (publicationRemoved.deletedCount == 0) {
+      return res.status(400).json({
+        status: "error",
+        message: "No se pudo eliminar o no existe",
+      });
+    }
+    return res.status(200).json({
+      status: "success",
+      message: "Mensaje Eliminado",
+      publicationRemoved,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "No se ha podido remover la publicación",
+      error: error,
+    });
+  }
+};
 
 // SUbir ficheros
+const uploader = async (req, res) => {
+  try {
+    //Obtener el id de la publicacion
+    const publicactionId = req.params.id;
+    //Recoger ficheroy ver si existe
+    if (!req.file) {
+      return res.status(404).json({
+        status: "error",
+        message: "Peticion no incluye la imagen",
+      });
+    }
+    let image = req.file.originalname;
+    // Hce validacion del archivo y si no es valido lo borra
+    const imageSplit = image.split(".");
+    const extension = imageSplit[1];
+    if (extension != "png" && extension != "jpg" && extension != "gif") {
+      const filePath = req.file.path;
+      const fileDeleted = fs.unlinkSync(filePath);
+      return res.status(400).json({
+        status: "error",
+        message: "Extensión del fichero invalido",
+      });
+    }
+
+    let publicationUpdated = await Publication.findOneAndUpdate(
+      {user: req.user.id, _id: publicactionId},
+      { file: req.file.filename },
+      { new: true }
+    );
+
+    res.status(200).json({
+      status: "success",
+      publication: publicationUpdated,
+      file: req.file,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Error en upload imagen",
+    });
+  }
+};
 
 // Devolver multimedia
 
@@ -76,5 +183,8 @@ const detail = async (req,res) => {
 module.exports = {
   pruebaPublication,
   save,
-  detail
+  detail,
+  remove,
+  user,
+  uploader
 };
--- routes/publication.js
+++ routes/publication.js
@@ -2,11 +2,27 @@ const express = require('express');
 const router = express.Router();
 const publicationControler = require('../controllers/publication.js');
 const check = require('../middlewars/auth.js');
+const multer = require('multer');
+
+//  Configuracion de subida
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/publications/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, "pub-"+Date.now()+ file.originalname)
+    }
+})
+
+const uploads = multer({storage});
 
 // Defino la rutas
 
 router.get('/', publicationControler.pruebaPublication );
 router.get('/detail/:id', check.auth, publicationControler.detail);
 router.post('/save',check.auth, publicationControler.save);
+router.delete('/delete/:id', check.auth, publicationControler.remove);
+router.get('/user/:id/:page?', check.auth, publicationControler.user);
+router.post('/upload/:id', [check.auth, uploads.single("file0")], publicationControler.uploader);
 
 module.exports = router;
